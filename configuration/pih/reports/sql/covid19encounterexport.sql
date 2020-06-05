8015929586e6468b9d82841728b6c6db637cf050
James Mbabazi <jmbabazi@pih.org>
2020-06-05 17:02:34 -0400

    Admission obs

--- configuration/pih/reports/sql/covid19encounterexport.sql
+++ configuration/pih/reports/sql/covid19encounterexport.sql
@@ -6,8 +6,11 @@
 ## THIS EXPECTS A startDate AND endDate PARAMETER IN ORDER TO RESTRICT BY ENCOUNTERS WITHIN A GIVEN DATE RANGE
 ## THE EVALUATOR WILL INSERT THESE AS BELOW WHEN EXECUTING.  YOU CAN UNCOMMENT THE BELOW LINES FOR MANUAL TESTING:
 
-## set @startDate='2020-05-01';
-## set @endDate='2020-05-31';
+# set @startDate='2020-05-01';
+# set @endDate='2020-05-31';
+
+## sql updates
+set sql_safe_updates = 0;
 
 ## CREATE SCHEMA FOR DATA EXPORT
 
@@ -25,7 +28,35 @@ create temporary table temp_encounter
     encounter_type      varchar(50),
     encounter_location  varchar(255),
     encounter_datetime  datetime,
-    encounter_provider  varchar(100)
+    encounter_provider  varchar(100),
+    health_care_worker  varchar(11),
+    pregnant            varchar(11),
+    last_menstruation_date  datetime,
+    estimated_delivery_date datetime,
+    postpartum_state varchar(255),
+    outcome varchar(100),
+    postpartum_state_1 varchar(255),
+    outcome_1 varchar(100),
+    date_of_delivery datetime,
+    home_medications text,
+    allergies text,
+    symptom_start_date datetime,
+    symptoms text,
+    other_symptoms text,
+    comorbidities varchar(11),
+    available_comorbidities text,
+    other_comorbidities text,
+    mental_health text,
+    smoker varchar(11),
+    transfer varchar(11),
+	transfer_facility varchar(255),
+	covid_case_contact varchar(11),
+    case_condition varchar(50),
+	temp double,
+	heart_rate double,
+	respiratory_rate double,
+	bp_systolic double,
+	bp_diastolic double
 );
 
 ## POPULATE WITH BASE DATA FROM ENCOUNTER, PATIENT, AND PERSON
@@ -83,7 +114,207 @@ update temp_encounter set encounter_provider = provider(encounter_id);
 update temp_encounter set encounter_location = encounter_location_name(encounter_id);
 
 # ADD OBSERVATIONS
-## TODO
+
+###DEMOGRAPHICS
+##ADMISSION FORM
+
+## Health Care Worker
+update temp_encounter set health_care_worker = obs_value_coded_list(
+     encounter_id,
+    'CIEL',
+    '5619',
+    'fr'
+);
+
+## Pregnancy
+####Pregnant
+update temp_encounter set pregnant = obs_value_coded_list(
+     encounter_id,
+    'CIEL',
+    '5272',
+    'fr'
+);
+
+-- last menstruation date
+update temp_encounter te left join obs o
+on  o.concept_id = concept_from_mapping('CIEL', '1427')
+and o.person_id = te.patient_id 
+and o.encounter_id = te.encounter_id 
+and o.voided = 0 
+set te.last_menstruation_date = o.value_datetime;
+
+-- estimated delivery date
+update temp_encounter te left join obs o
+on  o.concept_id = concept_from_mapping('CIEL', '5596')
+and o.person_id = te.patient_id 
+and o.encounter_id = te.encounter_id 
+and o.voided = 0 
+set te.estimated_delivery_date = o.value_datetime;
+
+-- postpartum state 
+update temp_encounter te left join obs o
+on  o.concept_id = concept_from_mapping('CIEL', '162747')
+and o.person_id = te.patient_id 
+and o.encounter_id = te.encounter_id 
+and o.voided = 0 
+and o.value_coded = concept_from_mapping('CIEL', '129317')
+set te.postpartum_state = concept_name(o.value_coded, 'fr');
+
+-- outcome
+update temp_encounter set outcome = obs_value_coded_list(
+     encounter_id,
+    'CIEL',
+    '161033',
+    'fr'
+);
+
+-- date of delivery
+update temp_encounter te left join obs o
+on  o.concept_id = concept_from_mapping('CIEL', '5599')
+and o.person_id = te.patient_id 
+and o.encounter_id = te.encounter_id 
+and o.voided = 0 
+set te.date_of_delivery = o.value_datetime;
+
+### HOME MEDICATION
+update temp_encounter set home_medications = obs_value_text(
+     encounter_id,
+    'CIEL',
+    '162165'
+);
+
+### ALLERGIES
+update temp_encounter set allergies = obs_value_text(
+     encounter_id,
+    'CIEL',
+    '162141'
+);
+
+## SIGNS AND SYMPTOMS
+-- start date
+update temp_encounter te left join obs o
+on  o.concept_id = concept_from_mapping('CIEL', '1730')
+and o.person_id = te.patient_id 
+and o.encounter_id = te.encounter_id 
+and o.voided = 0 
+set te.symptom_start_date = o.value_datetime;
+
+-- symptoms
+update temp_encounter set symptoms = obs_value_coded_list(
+     encounter_id,
+    'CIEL',
+    '1727', #1728
+    'fr'
+);
+
+-- other symptoms
+update temp_encounter set other_symptoms = obs_value_text(
+     encounter_id,
+    'CIEL',
+    '165996'
+);
+
+### COMORBIDITIES
+-- comorbidities
+update temp_encounter set comorbidities = obs_value_coded_list(
+     encounter_id,
+    'PIH',
+    '12976',
+    'fr'
+);
+
+-- comorbidities
+update temp_encounter set available_comorbidities = obs_value_coded_list(
+     encounter_id,
+    'CIEL',
+    '162747',
+    'fr'
+);
+
+-- other comorbidities
+update temp_encounter te
+left join obs o 
+on  o.person_id = te.patient_id 
+and o.encounter_id = te.encounter_id
+and o.voided = 0
+and o.concept_id = concept_from_mapping('CIEL', '162747')
+and o.value_coded = concept_from_mapping('CIEL','5622')
+set other_comorbidities = o.comments;
+
+update temp_encounter set mental_health = obs_value_text(
+	 encounter_id,
+    'CIEL',
+    '163044'
+);
+
+-- smoker
+update temp_encounter set smoker = obs_value_coded_list(
+     encounter_id,
+    'CIEL',
+    '163731',
+    'fr'
+);
+
+#### ADMISSION
+-- transfer
+update temp_encounter set transfer = obs_value_coded_list(
+     encounter_id,
+    'CIEL',
+    '160563',
+    'fr'
+);
+
+update temp_encounter set transfer_facility = obs_value_text(
+     encounter_id,
+    'CIEL',
+    '161550'
+);
+
+update temp_encounter set covid_case_contact = obs_value_coded_list(
+     encounter_id,
+    'CIEL',
+    '162633',
+    'fr'
+);
+
+update temp_encounter set case_condition = obs_value_coded_list(
+     encounter_id,
+    'CIEL',
+    '159640',
+    'fr'
+);
+
+### ViTALS
+update temp_encounter set temp = obs_value_numeric(
+     encounter_id,
+    'CIEL',
+    '5088'
+);
+
+update temp_encounter set heart_rate = obs_value_numeric(
+     encounter_id,
+    'CIEL',
+    '5087'
+);
+
+update temp_encounter set respiratory_rate = obs_value_numeric(
+     encounter_id,
+    'CIEL',
+    '5242'
+);
+
+update temp_encounter set bp_systolic = obs_value_numeric(
+     encounter_id,
+    'CIEL',
+    '5085'
+);
+
+update temp_encounter set bp_diastolic = obs_value_numeric(
+     encounter_id,
+    'CIEL',
+    '5086'
+);
+
 
 # EXECUTE SELECT TO EXPORT TABLE CONTENTS
 
@@ -98,6 +329,31 @@ SELECT e.encounter_id,
        e.encounter_type,
        e.encounter_location,
        e.encounter_datetime,
-       e.encounter_provider
-FROM temp_encounter e
-;
+       e.encounter_provider,
+       e.health_care_worker,
+       e.pregnant,
+       e.last_menstruation_date,
+       e.estimated_delivery_date,
+       e.postpartum_state,
+       e.outcome,
+       e.date_of_delivery,
+       e.home_medications,
+       e.allergies,
+       e.symptom_start_date,
+       e.symptoms,
+       e.other_symptoms,
+       e.comorbidities,
+       e.available_comorbidities,
+       e.other_comorbidities,
+       e.mental_health,
+       e.smoker,
+       e.transfer,
+	   e.transfer_facility,
+       e.covid_case_contact,
+       e.case_condition,
+	   e.temp,
+	   e.heart_rate,
+	   e.respiratory_rate,	
+	   e.bp_systolic,
+	   e.bp_diastolic
+FROM temp_encounter e;
