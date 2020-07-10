89eb2cedc16587815fe3bc73731377d4c2e0ec8f
James Mbabazi <jmbabazi@pih.org>
2020-07-10 12:43:57 -0400

    Adding sql target file for admission

--- configuration/pih/petl/jobs/covid19/disposition/target.sql
+++ configuration/pih/petl/jobs/covid19/disposition/target.sql
@@ -2,10 +2,10 @@ CREATE TABLE covid_disposition
 (
   encounter_id          INT PRIMARY KEY,
   encounter_type_id     INT,
-  patient_id 						INT,
+  patient_id            INT,
   encounter_date        DATE,
-  encounter_type				VARCHAR(255),
-  location						  TEXT,
-  disposition						VARCHAR(255),
-  discharge_condition		VARCHAR(255)
+  encounter_type        VARCHAR(255),
+  location              TEXT,
+  disposition           VARCHAR(255),
+  discharge_condition   VARCHAR(255)
 );
