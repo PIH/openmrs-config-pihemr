1958e09fb303563d890d1f613662da1cdd546e48
James Mbabazi <jmbabazi@pih.org>
2020-07-15 17:13:23 -0400

    COvid 19 Diagnoses (row per diagnoses)

---
+++ configuration/pih/petl/jobs/covid19/diagnoses/target.sql
@@ -0,0 +1,12 @@
+CREATE TABLE covid_diagnoses
+(
+  person_id,
+  encounter_id,
+  encounter_type,
+  location,
+  encounter_date,
+  diagnosis_order,
+  diagnosis,
+  diagnosis_confirmation,
+  covid19_diagnosis
+);
