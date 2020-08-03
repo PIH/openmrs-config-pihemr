fb601d7a25b894f42c2f663cd2b5723ecc72bb7e
James Mbabazi <jmbabazi@pih.org>
2020-08-03 11:09:38 -0400

    Adding index ascending and descending

--- configuration/pih/petl/jobs/covid19/disposition/target.sql
+++ configuration/pih/petl/jobs/covid19/disposition/target.sql
@@ -6,5 +6,7 @@ CREATE TABLE covid_disposition
   location              TEXT,
   encounter_date        DATE,
   disposition           VARCHAR(255),
-  discharge_condition   VARCHAR(255)
+  discharge_condition   VARCHAR(255),
+  index_asc 		    INT,
+  index_desc 		    INT
 );
