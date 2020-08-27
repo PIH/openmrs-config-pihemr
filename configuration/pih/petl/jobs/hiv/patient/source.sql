3cc66156da370d51bd6321722719c7c4228cc288
mgoodrich <mark.goodrich@live.com>
2020-08-27 17:17:27 -0400

    UHM-4780: Establish PETL pipeline for Haiti HIV environment fix query

--- configuration/pih/petl/jobs/hiv/patient/source.sql
+++ configuration/pih/petl/jobs/hiv/patient/source.sql
@@ -6,6 +6,15 @@ SELECT
     pn.family_name,
     per.gender,
     per.birthdate
-FROM patient pat, person per, person_name pn
-WHERE pat.patient_id = pn.person_id and pat.patient_id = per.person_id;
+FROM
+    patient pat, person per, person_name pn
+WHERE
+    pat.patient_id = pn.person_id and
+    pat.patient_id = per.person_id and
+    pat.voided = 0 and
+    per.voided = 0 and
+    pn.voided = 0 and
+    pn.preferred = 1
+
+
 
