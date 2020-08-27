8720e6518f6f78266e28fbd8e233bbb91fd22bfe
mgoodrich <mark.goodrich@live.com>
2020-08-27 15:38:02 -0400

    UHM-4780: Establish PETL pipeline for Haiti HIV environment

---
+++ configuration/pih/petl/jobs/hiv/patient/target.sql
@@ -0,0 +1,8 @@
+create table hiv_patient
+(
+    patient_id      INT PRIMARY KEY,
+    given_name      VARCHAR(50),
+    family_name     VARCHAR(50),
+    gender          VARCHAR(50),
+    birthdate       DATE
+);
