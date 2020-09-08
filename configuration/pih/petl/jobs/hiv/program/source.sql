16cb20303cb9e95e30465f1b531a729ab872fa3d
mgoodrich <mark.goodrich@live.com>
2020-09-08 17:21:19 -0400

    UHM-4810: Create "hiv_patient_program" table in PETL

--- configuration/pih/petl/jobs/hiv/program/source.sql
+++ configuration/pih/petl/jobs/hiv/program/source.sql
@@ -1,6 +1,6 @@
-DROP TABLE IF EXISTS temp_patient_program;
+DROP TABLE IF EXISTS temp_hiv_patient_program;
 
-CREATE TABLE temp_patient_program
+CREATE TABLE temp_hiv_patient_program
 (
   patient_program_id int(11),
   patient_id int(11),
@@ -12,15 +12,15 @@ CREATE TABLE temp_patient_program
   outcome varchar(255)
 );
 
-INSERT into temp_patient_program (patient_program_id, patient_id, date_enrolled, date_completed, location_id,outcome_concept_id)
+INSERT into temp_hiv_patient_program (patient_program_id, patient_id, date_enrolled, date_completed, location_id,outcome_concept_id)
 select patient_program_id, patient_id, date_enrolled, date_completed,location_id,outcome_concept_id
     from patient_program
     where voided=0
     and program_id = (select program_id from program where uuid='b1cb1fc1-5190-4f7a-af08-48870975dafc');
 
-update temp_patient_program
+update temp_hiv_patient_program
 set location = location_name(location_id),
     outcome = concept_name(outcome_concept_id, 'en');
 
-select patient_program_id, patient_id, date_enrolled, date_completed, location, outcome from temp_patient_program;
+select patient_program_id, patient_id, date_enrolled, date_completed, location, outcome from temp_hiv_patient_program;
 
