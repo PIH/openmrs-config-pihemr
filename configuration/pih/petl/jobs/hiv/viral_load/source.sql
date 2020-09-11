70de6c8bf12c5ed67269408361174b8e14790de5
James Mbabazi <jmbabazi@pih.org>
2020-09-11 15:46:26 -0400

    Rectifying indexes and changes per reviews

--- configuration/pih/petl/jobs/hiv/viral_load/source.sql
+++ configuration/pih/petl/jobs/hiv/viral_load/source.sql
@@ -3,8 +3,8 @@
 SET sql_safe_updates = 0;
 
 DROP TEMPORARY TABLE IF EXISTS temp_hiv_construct_encounters;
-DROP TEMPORARY TABLE IF EXISTS temp_index_asc;
-DROP TEMPORARY TABLE IF EXISTS temp_index_desc;
+DROP TEMPORARY TABLE IF EXISTS temp_vl_index_asc;
+DROP TEMPORARY TABLE IF EXISTS temp_vl_index_desc;
 
 ### hiv constructs table
 CREATE TEMPORARY TABLE temp_hiv_construct_encounters
@@ -12,6 +12,7 @@ CREATE TEMPORARY TABLE temp_hiv_construct_encounters
     patient_id                      INT,
     encounter_id                    INT,
     vl_sample_taken_date            DATETIME,
+    date_created                    DATETIME,
     vl_sample_taken_date_estimated  VARCHAR(11),
     vl_result_date                  DATE,
     vl_test_outcome                 VARCHAR(255),
@@ -25,8 +26,22 @@ INSERT INTO temp_hiv_construct_encounters (patient_id, encounter_id)
 SELECT person_id, encounter_id FROM obs WHERE voided = 0 AND concept_id = CONCEPT_FROM_MAPPING("PIH", "HIV viral load construct");
 
 -- specimen collection date
-UPDATE temp_hiv_construct_encounters tvl JOIN encounter e ON tvl.encounter_id = e.encounter_id and e.voided = 0
+UPDATE temp_hiv_construct_encounters tvl JOIN encounter e ON tvl.encounter_id = e.encounter_id
 SET	vl_sample_taken_date = e.encounter_datetime;
+
+-- date encounter was created
+UPDATE temp_hiv_construct_encounters tvl JOIN encounter e ON tvl.encounter_id = e.encounter_id
+SET	tvl.date_created = e.date_created;
+
+## Delete test patients
+DELETE FROM temp_hiv_construct_encounters WHERE
+patient_id IN (
+               SELECT
+                      a.person_id
+                      FROM person_attribute a
+                      INNER JOIN person_attribute_type t ON a.person_attribute_type_id = t.person_attribute_type_id
+                      AND a.value = 'true' AND t.name = 'Test Patient'
+               );
 
 -- is specimen collection date estimated
 UPDATE temp_hiv_construct_encounters SET vl_sample_taken_date_estimated =  OBS_VALUE_CODED_LIST(encounter_id, 'PIH', '11781', 'en');
@@ -42,45 +57,47 @@ UPDATE temp_hiv_construct_encounters SET viral_load =  OBS_VALUE_NUMERIC(encount
 
 -- The indexes are calculated using the specimen collection date
 ### index ascending
-CREATE TEMPORARY TABLE temp_index_asc
+CREATE TEMPORARY TABLE temp_vl_index_asc
 (
     SELECT
             patient_id,
             vl_sample_taken_date,
+            date_created,
             encounter_id,
             index_asc
 FROM (SELECT
             @r:= IF(@u = patient_id, @r + 1,1) index_asc,
             vl_sample_taken_date,
+            date_created,
             encounter_id,
             patient_id,
             @u:= patient_id
       FROM temp_hiv_construct_encounters,
                     (SELECT @r:= 1) AS r,
                     (SELECT @u:= 0) AS u
-            ORDER BY patient_id, vl_sample_taken_date ASC
+            ORDER BY patient_id, vl_sample_taken_date ASC, date_created ASC
         ) index_ascending );
 
 ### index descending
-CREATE TEMPORARY TABLE temp_index_desc
+CREATE TEMPORARY TABLE temp_vl_index_desc
 (
     SELECT
             patient_id,
             vl_sample_taken_date,
+            date_created,
             encounter_id,
-            ## days since last viral load
-            DATEDIFF(NOW(), vl_sample_taken_date) days_since_last_vl,
             index_desc
 FROM (SELECT
             @r:= IF(@u = patient_id, @r + 1,1) index_desc,
             vl_sample_taken_date,
+            date_created,
             encounter_id,
             patient_id,
             @u:= patient_id
       FROM temp_hiv_construct_encounters,
                     (SELECT @r:= 1) AS r,
                     (SELECT @u:= 0) AS u
-            ORDER BY patient_id, vl_sample_taken_date DESC
+            ORDER BY patient_id, vl_sample_taken_date DESC, date_created DESC
         ) index_descending );
 
 ### Final query
@@ -94,12 +111,12 @@ SELECT
         vl_result_detectable,
         viral_load,
         vl_type,
-        IF(index_desc = 1, tid.days_since_last_vl, NULL) days_since_last_vl,
+        DATEDIFF(NOW(), tvl.vl_sample_taken_date) days_since_vl,
         index_desc,
         index_asc
 FROM temp_hiv_construct_encounters tvl
--- index ascending
-JOIN temp_index_asc tia ON tvl.encounter_id = tia.encounter_id
 -- index descending
-JOIN temp_index_desc tid ON tvl.encounter_id = tid.encounter_id
+JOIN temp_vl_index_desc tid ON tvl.encounter_id = tid.encounter_id
+-- index ascending
+JOIN temp_vl_index_asc tia ON tvl.encounter_id = tia.encounter_id
 ORDER BY tvl.patient_id, index_desc;
