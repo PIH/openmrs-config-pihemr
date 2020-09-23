25f0efe05948b577ed9824b721a8eea8c509bf0f
dmdesimone <ddesimone@pih.org>
2020-09-22 23:59:55 -0400

    tweaks for performance
    change how current_art_line_start_date is calculated due to performance issues

--- configuration/pih/petl/jobs/hiv/dispensing/source.sql
+++ configuration/pih/petl/jobs/hiv/dispensing/source.sql
@@ -38,7 +38,7 @@ regimen_match char(1)
  
  insert into temp_HIV_dispensing (patient_id, encounter_id, dispense_date, encounter_location_id)
  select patient_id, encounter_id,encounter_datetime,location_id from encounter
- where encounter_type = @HIV_dispensing and voided = 0
+ where encounter_type = @HIV_dispensing and voided = 0 
  ;
  
  update temp_HIV_dispensing
@@ -59,10 +59,10 @@ set dac = CASE when obs_single_value_coded(encounter_id, 'PIH', 3671,'PIH',9361)
 
 update temp_HIV_dispensing 
 set dispense_date_ascending = encounter_index_asc(encounter_id,'HIV drug dispensing',null,null);
- 
+
 update temp_HIV_dispensing 
 set dispense_date_descending = encounter_index_desc(encounter_id,'HIV drug dispensing',null,null);
- 
+
 update temp_HIV_dispensing 
 set dispensed_to = obs_value_coded_list(encounter_id,'PIH',12071,'en');
 
@@ -84,26 +84,26 @@ set is_current_mmd = if(months_dispensed >= 3, 'Y','N');
 drop temporary table if exists dup_HIV_dispensing;
 CREATE TEMPORARY TABLE dup_HIV_dispensing SELECT * FROM temp_HIV_dispensing;
 
--- to get the start date of the current line, first the max date of all the other lines is updated on that row
--- the art start date for that line is the minimum date of the rows of the current line that are greater than the max of the other lines 
--- this accounts for instances where a patients switch back and forth between lines
+drop temporary table if exists temp_dispensing_line_start;
+CREATE TEMPORARY TABLE temp_dispensing_line_start
+SELECT
+            @ts:= IF(@u = patient_id and @tl = current_art_treatment_line ,@ts,@ts:=dispense_date) treatment_start_date,
+            dispense_date,
+            encounter_id,
+            patient_id,
+            current_art_treatment_line,
+            @u:= patient_id,
+            @tl:=current_art_treatment_line
+      FROM temp_HIV_dispensing,
+                    (SELECT @ts:= '1900-01-01') AS ts,
+                    (SELECT @u:= 0) AS u,
+                    (SELECT @tl:='1900-01-01') as tl
+            ORDER BY patient_id, dispense_date ASC, encounter_id ASC;
 
 update temp_HIV_dispensing t
-set t.max_other_line = 
-     (select max(d.dispense_date) from dup_HIV_dispensing d
-     where d.patient_id = t.patient_id
-     and d.current_art_treatment_line <> t.current_art_treatment_line
-     and d.dispense_date_ascending <t.dispense_date_ascending)
- ;    
-
-update temp_HIV_dispensing t
-set t.current_art_line_start_date =
-    (select min(d.dispense_date) from dup_HIV_dispensing d
-     where d.patient_id = t.patient_id
-     and d.current_art_treatment_line = t.current_art_treatment_line
-     and (d.dispense_date >= t.max_other_line or t.max_other_line is null)
-    ); 
-
+inner join temp_dispensing_line_start ts on ts.encounter_id = t.encounter_id
+set t.current_art_line_start_date = ts.treatment_start_date;
+ 
 -- ARV#1 med and quantity
 update temp_HIV_dispensing 
 set arv_1_med_short_name = obs_from_group_id_value_coded_list(obs_group_id_of_coded_answer(encounter_id,'PIH',3013),'CIEL','1282','en');
@@ -134,35 +134,11 @@ set tms_1_med = obs_from_group_id_value_drug(obs_group_id_of_coded_answer(encoun
 update temp_HIV_dispensing 
 set tms_1_quantity = obs_from_group_id_value_numeric(obs_group_id_of_coded_answer(encounter_id,'PIH',3120),'CIEL','1443');
 
--- to get the start date of the current line, first the max date of all the other lines is updated on that row
--- the art start date for that line is the minimum date of the rows of the current line that are greater than the max of the other lines 
--- this accounts for instances where a patients switch back and forth between lines
--- Also, it creates a duplicate of the temp table to get around the MYSQL restriction of joining in a table that's also being updated 
-
-drop temporary table if exists dup_HIV_dispensing;
-CREATE TEMPORARY TABLE dup_HIV_dispensing SELECT * FROM temp_HIV_dispensing;
 
 update temp_HIV_dispensing t
-set t.max_other_line = 
-     (select max(d.dispense_date) from dup_HIV_dispensing d
-     where d.patient_id = t.patient_id
-     and d.current_art_treatment_line <> t.current_art_treatment_line
-     and d.dispense_date_ascending <t.dispense_date_ascending)
- ;    
-
-update temp_HIV_dispensing t
-set t.current_art_line_start_date =
-    (select min(d.dispense_date) from dup_HIV_dispensing d
-     where d.patient_id = t.patient_id
-     and d.current_art_treatment_line = t.current_art_treatment_line
-     and (d.dispense_date >= t.max_other_line or t.max_other_line is null)
-    ); 
-
-
-update temp_HIV_dispensing t
-left outer join dup_HIV_dispensing d on d.patient_id=t.patient_id and d.dispense_date_descending = 1  
-set t.regimen_change = if(d.arv_1_med_short_name <> t.arv_1_med_short_name,1,0)  -- need to change this to FULL NAME when drugs are captured?
-where t.dispense_date_ascending = 1
+left outer join dup_HIV_dispensing d on d.patient_id=t.patient_id and d.dispense_date_ascending = 1  
+set t.regimen_change = if(d.arv_1_med_short_name = t.arv_1_med_short_name,0,1)  -- need to change this to FULL NAME when drugs are captured?
+where t.dispense_date_descending = 1
 ;
 
 update temp_HIV_dispensing t
@@ -171,32 +147,32 @@ set t.days_late_to_pickup = if(t.dispense_date>d.next_dispense_date,datediff(t.d
 where t.dispense_date_descending = 1;
 
 Select 
-patient_id,
-encounter_id,
-dispense_date,
-dispense_site,
-age_at_dispense_date,
-dac,
-dispensed_to,
-dispensed_accompagnateur,
-current_art_treatment_line,
-current_art_line_start_date,
-months_dispensed,
-is_current_mmd,
-next_dispense_date,
-arv_1_med_short_name,
-arv_1_med,
-arv_1_quantity,
-arv_2_med_short_name,
-arv_2_med,
-arv_2_quantity,
-tms_1_med_short_name,
-tms_1_med,
-tms_1_quantity,
-regimen_change,
-days_late_to_pickup,
-regimen_match,
+t.patient_id,
+t.encounter_id,
+t.dispense_date,
+t.dispense_site,
+t.age_at_dispense_date,
+t.dac,
+t.dispensed_to,
+t.dispensed_accompagnateur,
+t.current_art_treatment_line,
+t.current_art_line_start_date,
+t.months_dispensed,
+t.is_current_mmd,
+t.next_dispense_date,
+t.arv_1_med_short_name,
+t.arv_1_med,
+t.arv_1_quantity,
+t.arv_2_med_short_name,
+t.arv_2_med,
+t.arv_2_quantity,
+t.tms_1_med_short_name,
+t.tms_1_med,
+t.tms_1_quantity,
+t.regimen_change,
+t.days_late_to_pickup,
+t.regimen_match,
 dispense_date_ascending,
 dispense_date_descending
-from temp_HIV_dispensing
+from temp_HIV_dispensing t 
 order by patient_id, dispense_date asc, encounter_id asc;
