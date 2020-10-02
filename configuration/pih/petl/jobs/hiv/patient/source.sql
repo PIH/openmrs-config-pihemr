10d552cd077518a9f75083e1ee2ca3c203164c0a
James Mbabazi <jmbabazi@pih.org>
2020-10-02 09:49:57 -0400

    Adding causing of death to the patient table

--- configuration/pih/petl/jobs/hiv/patient/source.sql
+++ configuration/pih/petl/jobs/hiv/patient/source.sql
@@ -1,16 +1,20 @@
+SET sql_safe_updates = 0;
+
 DROP TABLE IF EXISTS temp_patient;
 
 CREATE TABLE temp_patient
 (
-    patient_id int(11),
-    given_name varchar(50),
-    family_name varchar(50),
-    gender varchar(50),
-    birthdate date
+    patient_id                  INT(11),
+    given_name                  VARCHAR(50),
+    family_name                 VARCHAR(50),
+    gender                      VARCHAR(50),
+    birthdate                   DATE,
+    cause_of_death              VARCHAR(255),
+    cause_of_death_non_coded    VARCHAR(255)
 );
 
-INSERT into temp_patient (patient_id)
-select patient_id from patient where voided=0;
+INSERT INTO temp_patient (patient_id)
+SELECT patient_id FROM patient WHERE voided=0;
 
 ## Delete test patients
 DELETE FROM temp_patient WHERE
@@ -22,10 +26,19 @@ patient_id IN (
                       AND a.value = 'true' AND t.name = 'Test Patient'
                );
 
-update temp_patient 
-set gender = gender(patient_id),
-    birthdate = birthdate(patient_id),
-    given_name = person_given_name(patient_id),
-    family_name = person_family_name(patient_id);
+UPDATE temp_patient
+SET gender = GENDER(patient_id),
+    birthdate = BIRTHDATE(patient_id),
+    given_name = PERSON_GIVEN_NAME(patient_id),
+    family_name = PERSON_FAMILY_NAME(patient_id);
 
-select * from temp_patient;
+# Cause of death
+UPDATE temp_patient tp INNER JOIN person p ON tp.patient_id = p.person_id and p.dead = concept_from_mapping("PIH","YES")
+SET tp.cause_of_death = concept_from_mapping(p.cause_of_death, 'en');
+
+# Cause of death non coded
+UPDATE temp_patient tp INNER JOIN person p ON tp.patient_id = p.person_id and p.dead = concept_from_mapping("PIH","YES")
+SET tp.cause_of_death_non_coded = concept_from_mapping(p.cause_of_death_non_coded, 'en');
+
+### Final Query
+SELECT * FROM temp_patient;
--- configuration/pih/petl/jobs/hiv/patient/target.sql
+++ configuration/pih/petl/jobs/hiv/patient/target.sql
@@ -1,8 +1,10 @@
 create table hiv_patient
 (
-    patient_id      INT PRIMARY KEY,
-    given_name      VARCHAR(50),
-    family_name     VARCHAR(50),
-    gender          VARCHAR(50),
-    birthdate       DATE
+    patient_id                  INT PRIMARY KEY,
+    given_name                  VARCHAR(50),
+    family_name                 VARCHAR(50),
+    gender                      VARCHAR(50),
+    birthdate                   DATE,
+    cause_of_death              VARCHAR(255),
+    cause_of_death_non_coded    VARCHAR(255)
 );
