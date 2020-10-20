ce72a6c12b8d6eb81f136fb8f4d7d09d0d77c2da
James Mbabazi <jmbabazi@pih.org>
2020-10-20 15:45:35 -0400

    Adding test not performed

--- configuration/pih/petl/jobs/labs/results/target.sql
+++ configuration/pih/petl/jobs/labs/results/target.sql
@@ -1,14 +1,17 @@
 CREATE TABLE lab_results
 (
-patient_id                  INT,
-encounter_id                INT,
-specimen_collection_date    DATE,
-sample_taken_date_estimated VARCHAR(11),
-test_result_date            DATE,
-test_related_to             VARCHAR(25),
-test_type                   VARCHAR(255),
-test_result_text            VARCHAR(255),
-test_result_numeric         FLOAT,
-index_asc                   INT,
-index_desc                  INT
+    patient_id                  INT,
+    encounter_id                INT,
+    specimen_collection_date    DATE,
+    sample_taken_date_estimated VARCHAR(11),
+    test_result_date            DATE,
+    test_related_to             VARCHAR(25),
+    test_type                   VARCHAR(255),
+    test_status                 VARCHAR(255),
+    reason_test_not_perform     VARCHAR(255),
+    test_result_text            VARCHAR(255),
+    test_result_numeric         FLOAT,
+    index_asc                   INT,
+    index_desc                  INT,
+    date_created                DATETIME
 );
