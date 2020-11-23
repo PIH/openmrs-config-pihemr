8c8d0cc8b27ac133826d5479d2d32735174f1088
James Mbabazi <jmbabazi@pih.org>
2020-11-23 16:21:13 -0500

    cron job

--- configuration/pih/petl/jobs/hiv/ovc/ovc_program/target.sql
+++ configuration/pih/petl/jobs/hiv/ovc/ovc_program/target.sql
@@ -2,8 +2,8 @@ CREATE TABLE ovc_program
 (
     patient_program_id  INT,
     zlemr_id            VARCHAR(50),
-    patient_id      INT,
-    date_enrolled   DATE,
+    patient_id          INT,
+    date_enrolled       DATE,
     date_completed      DATE,
     location            VARCHAR(255),
     program_status      VARCHAR(255),
