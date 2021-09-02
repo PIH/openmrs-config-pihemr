df044562b5563a67b75cb8b08f9a3dfa5431a86b
James Mbabazi <jmbabazi@pih.org>
2021-09-02 12:42:50 -0400

    Changing column names for datakind_patient_program

--- configuration/pih/petl/jobs/data_kind/patient_program/target.sql
+++ configuration/pih/petl/jobs/data_kind/patient_program/target.sql
@@ -1,10 +1,10 @@
 CREATE TABLE datakind_patient_program
 (
     patient_id      INT,
-    pp.name         VARCHAR(100),
+    program_name            VARCHAR(100),
     date_enrolled   DATETIME,
     date_completed  DATETIME,
     outcome         VARCHAR(100),
-    p.date_created  DATETIME,
-    p.date_changed  DATETIME
+    date_created    DATETIME,
+    date_changed    DATETIME
 );
