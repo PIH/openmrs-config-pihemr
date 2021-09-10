0c5f4d16cb7df0af8fc3bc417b48facdd9f800ed
Michael Seaton <mseaton@pih.org>
2021-09-10 08:12:49 -0400

    UHM-5710 - Limit program enrollment checkboxes such that only one can be selected at a time. (#253)

--- configuration/pih/scripts/global/mch.js
+++ configuration/pih/scripts/global/mch.js
@@ -104,3 +104,21 @@ function setUpNextButtonForSections(currentSection) {
     window.htmlForm.submitHtmlForm();
   });
 }
+
+function setupCheckboxRadioGroups() {
+  jq(".checkbox-radio-group").each(function(groupIndex, groupElement) {
+    const options = jq(groupElement).find(".checkbox-radio-option");
+    jq(options).each(function(index1, option1) {
+      const checkbox = jq(option1).find("input:checkbox");
+      jq(checkbox).change(function(event) {
+        if(jq(checkbox).is(":checked")) {
+          jq(options).each(function(index2, option2) {
+            if (index1 !== index2) {
+              jq(option2).find("input:checkbox").prop("checked", false);
+            }
+          });
+        }
+      });
+    });
+  });
+}
