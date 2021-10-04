b40a78bc9e08d5c8eee9e19335ff5ae56780c896
dmdesimone <ddesimone@pih.org>
2021-10-03 20:56:09 -0400

    rewrite allpatientswithids
    Rewrite allpatientswithids as it was failing in HUM prod

--- configuration/reports/reportdescriptors/dataexports/sql/allpatientswithids.sql
+++ configuration/reports/reportdescriptors/dataexports/sql/allpatientswithids.sql
@@ -1,70 +1,111 @@
-select 1;
-/**
 CALL initialize_global_metadata();
 
-select pn.family_name as 'Nom', pn.given_name as 'Prénom',
-       pa.state_province as Department,
-       pa.city_village as Commune,
-       pa.address3 as Section,
-       pa.address1 as Locality,
-       pa.address2 as StreetLandmark,
-       p.birthdate as 'Date de naissance',
-       p.birthdate_estimated as 'Date de naissance estimé',
-       CAST(CONCAT(timestampdiff(YEAR, p.birthdate, NOW()), '.', MOD(timestampdiff(MONTH, p.birthdate, NOW()), 12) ) as CHAR) as Age,
-       p.gender as Sexe,
-       zl.identifier as 'ZL ID',
-       dos.identifier as Dossier,
-       t.value as TelephoneNumber,
-       ahe_section.user_generated_id as 'Section Communale CDC ID',
-       bio.date_created "Last Biometric Date"
+DROP TEMPORARY TABLE IF EXISTS temp_patients;
+CREATE TEMPORARY TABLE temp_patients
+(
+       patient_id					int(11),
+	family_name					varchar(255),
+	given_name					varchar(255),
+       patient_address_level_1                   varchar(255),
+       patient_address_level_2                   varchar(255),  
+       patient_address_level_3                   varchar(255),
+       patient_address_level_4                   varchar(255),
+       patient_address_level_5                   varchar(255),
+       birthdate					datetime,
+       birthdate_estimated                       char(1),
+       age						decimal,
+	gender						char(1),
+       patient_primary_id                        varchar(50),
+       dossier_id					varchar(50),
+	telephone_number			       varchar(50),
+	Section_Communale_CDC_ID	              varchar(11),
+	last_biometric_date			       datetime
+	);	
+	
+insert into temp_patients (patient_id)
+select patient_id from patient where voided = 0;
 
-  from patient pat
+-- patient name
+update temp_patients t
+inner join person_name pn on person_name_id =
+	(select person_name_id from person_name pn2
+	where pn2.person_id = t.patient_id
+	and pn2.voided = 0
+	order by preferred desc, date_created desc limit 1)
+set t.family_name = pn.family_name,
+	t.given_name = pn.given_name; 
 
- inner join person p
-    on pat.patient_id = p.person_id
-   and p.voided = 0
 
- inner join person_name pn
-    on pat.patient_id = pn.person_id
-   and pn.voided = 0
+-- person table fields
+update temp_patients t 
+inner join person p on p.person_id = t.patient_id
+set t.birthdate = p.birthdate, 
+	t.birthdate_estimated = p.birthdate_estimated,
+	t.gender = p.gender,
+    t.age = CAST(CONCAT(timestampdiff(YEAR, p.birthdate, NOW()), '.', MOD(timestampdiff(MONTH, p.birthdate, NOW()), 12) ) as CHAR)
+;
 
- left outer join person_address pa
-    on pat.patient_id = pa.person_id
+-- primary identifier
+update temp_patients set patient_primary_id = patient_identifier(patient_id, metadata_uuid('org.openmrs.module.emrapi', 'emr.primaryIdentifierType'));
+
+-- dossier id
+select patient_identifier_type_id into @dossier from patient_identifier_type pit where uuid = 'e66645eb-03a8-4991-b4ce-e87318e37566';
+update temp_patients set dossier_id = patient_identifier(patient_id, 'e66645eb-03a8-4991-b4ce-e87318e37566');
+
+-- telephone number
+update temp_patients t 
+ inner join person_attribute pa on t.patient_id = pa.person_id
    and pa.voided = 0
+   and pa.person_attribute_type_id = @phoneNumber
+ set t.telephone_number = pa.value;  
 
- left outer join person_attribute t
-    on pat.patient_id = t.person_id
-   and t.voided = 0
-   and t.person_attribute_type_id = @phoneNumber
-
- left outer join patient_identifier dos
-   on pat.patient_id = dos.patient_id
-  and identifier_type = @dosId
-  and dos.voided = 0
-
-
- left outer join patient_identifier zl
-   on pat.patient_id = zl.patient_id
-  and zl.identifier_type = @zlId
-  and zl.voided = 0
-  and zl.preferred= 1
-
- left outer join patient_identifier bio on bio.patient_identifier_id =
-    (select patient_identifier_id from patient_identifier bio2
-    where pat.patient_id = bio2.patient_id
-    and bio2.identifier_type = @biometricId
-    and bio2.voided = 0
-    order by bio2.date_created desc limit 1)
-
+-- patient address
+-- *NOTE* it seems that CDC ID is not working 
+update temp_patients t 
+INNER JOIN person_address pa on pa.person_address_id =
+	(select person_address_id from person_address pa2
+	where pa2.person_id = t.patient_id
+	and pa2.voided = 0
+	order by preferred desc, date_created desc limit 1)
 LEFT OUTER JOIN address_hierarchy_entry ahe_country on ahe_country.level_id = 1 and ahe_country.name = pa.country
 LEFT OUTER JOIN address_hierarchy_entry ahe_dept on ahe_dept.level_id = 2 and ahe_dept.parent_id = ahe_country.address_hierarchy_entry_id and ahe_dept.name = pa.state_province
 LEFT OUTER JOIN address_hierarchy_entry ahe_commune on ahe_commune.level_id = 3 and ahe_commune.parent_id = ahe_dept.address_hierarchy_entry_id and ahe_commune.name = pa.city_village
 LEFT OUTER JOIN address_hierarchy_entry ahe_section on ahe_section.level_id = 4 and ahe_section.parent_id = ahe_commune.address_hierarchy_entry_id and ahe_section.name = pa.address3
+set patient_address_level_1 = pa.state_province,
+	patient_address_level_2 = pa.city_village,
+	patient_address_level_3 = pa.address3,
+	patient_address_level_4 = pa.address1,
+	patient_address_level_5 = pa.address2,
+	Section_Communale_CDC_ID = ahe_section.user_generated_id;
+
+-- commenting out this statement for biometric ID date, because it performs really porrly
+/*
+update temp_patients t 
+inner join patient_identifier bio on bio.patient_identifier_id =
+    (select patient_identifier_id from patient_identifier bio2
+    where t.patient_id = bio2.patient_id
+    and bio2.identifier_type = @biometricId
+    and bio2.voided = 0 
+    order by date_created desc limit 1)
+set t.last_biometric_date = bio.date_created; 
+*/    
 
 
-where pat.voided = 0
-
-group by p.person_id, pn.family_name, pn.given_name, p.gender, p.birthdate
-
-order by pn.family_name, pn.given_name, p.gender, p.birthdate;
-**/
+SELECT 
+       family_name,
+       given_name,
+       patient_address_level_1,
+       patient_address_level_2,  
+       patient_address_level_3,
+       patient_address_level_4,
+       patient_address_level_5,
+       birthdate,
+       birthdate_estimated,
+       age,
+       gender,
+       patient_primary_id,
+       dossier_id,
+       telephone_number,
+       Section_Communale_CDC_ID,
+       last_biometric_date
+from temp_patients;
