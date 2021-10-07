48c4564011e39618f6a62e2a5a5ab9fa261417f4
James Mbabazi <jmbabazi@pih.org>
2021-10-07 16:38:46 -0400

    Adding hivemr-v1 to the transitional register

--- configuration/reports/reportdescriptors/dataexports/sql/transitionalRegister.sql
+++ configuration/reports/reportdescriptors/dataexports/sql/transitionalRegister.sql
@@ -8,7 +8,8 @@ p.patient_id NOT IN (SELECT patient_id FROM patient_program pp WHERE pp.voided =
 SELECT 
     p.patient_id,
     zlemr,
-    identifier hiv_emr,
+    pii.identifier hivemr_v1,
+    pi.identifier hivemr_dosid,
     DATE(e.encounter_datetime) patient_registration_date,
     LOCATION_NAME(e.location_id) patient_registration_location
 FROM
@@ -19,4 +20,9 @@ FROM
         AND encounter_type = @regEnc
         LEFT JOIN
     patient_identifier pi ON pi.patient_id = p.patient_id
-        AND pi.identifier_type = @hivDosId;
+        AND pi.voided = 0
+        AND pi.identifier_type = @hivDosId
+        LEFT JOIN
+    patient_identifier pii ON pii.patient_id = p.patient_id
+        AND pii.voided = 0
+        AND pii.identifier_type = @hivId;
