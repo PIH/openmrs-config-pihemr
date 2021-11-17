092dfdd23d333b49a4011de486f8bff38f11c8ca
James Mbabazi <jmbabazi@pih.org>
2021-11-17 14:55:14 -0500

    moving the petl folder to the zl repo

--- configuration/pih/petl/jobs/data_kind/registration_activity/target.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE datakind_registration_activity
-(
-            person_id       INT,
-            date_created    DATETIME,
-            date_changed    DATETIME,
-            table_name      VARCHAR(15),
-            index_count     INT
-);
