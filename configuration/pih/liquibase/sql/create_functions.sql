ff0947d3f90ee032af0dbb53d5a40a54f5fe6d80
dmdesimone <ddesimone@pih.org>
2022-06-06 23:01:04 -0400

    add temp_obs functions for mch_delivery
    add temp_obs functions for mch_delivery

--- configuration/pih/liquibase/sql/create_functions.sql
+++ configuration/pih/liquibase/sql/create_functions.sql
@@ -2827,3 +2827,135 @@ BEGIN
 
 END
 #
+-- This function accepts encounter_id, mapping source, mapping code (for both the concept_id and value_coded) and returns
+-- "yes" (in the default locale of the implementation) if the obs_id exists and
+-- null if the obs_id does not exisit
+-- This function is used on questions that also act as answers (i.e you either check it as true or your don't)
+-- this function runs against the temp_obs table
+#
+DROP FUNCTION IF EXISTS obs_single_value_coded_from_temp;
+#
+CREATE FUNCTION obs_single_value_coded_from_temp(_encounterId int(11), _source varchar(50), _term varchar(255), _source1 varchar(50), _term1 varchar(255))
+RETURNS varchar(11)
+DETERMINISTIC
+
+BEGIN
+
+DECLARE ret varchar(11);
+
+select      IFNULL(NULL, concept_name(concept_from_mapping('PIH','YES'), global_property_value('default_locale', 'en'))) into ret FROM
+(
+select      obs_id
+from        temp_obs o
+where       o.voided = 0
+and         o.encounter_id = _encounterId
+and         o.concept_id = concept_from_mapping(_source, _term)
+and         o.value_coded = concept_from_mapping(_source1, _term1)
+order by    o.date_created desc, o.obs_id desc
+limit 1
+) obs_single_question_answer;
+
+RETURN ret;
+
+END
+#
+-- This function accepts obs_group_id, mapping source, mapping code
+-- It will find the comments entry that matches this from the temp_obs table
+#
+DROP FUNCTION IF EXISTS obs_from_group_id_comment_from_temp;
+#
+CREATE FUNCTION obs_from_group_id_comment_from_temp(_obsGroupId int(11), _source varchar(50), _term varchar(255))
+    RETURNS text
+    DETERMINISTIC
+
+BEGIN
+
+    DECLARE ret text;
+
+    select      comments into ret
+    from        temp_obs o
+    where       o.voided = 0
+      and       o.obs_group_id= _obsGroupId
+      and       o.concept_id = concept_from_mapping(_source, _term);
+
+    RETURN ret;
+
+END
+#
+-- This function accepts encounter_id, mapping source, mapping code
+-- It will find a single, best observation that matches this, and return the comment
+-- this function runs against the temp_obs table
+#
+DROP FUNCTION IF EXISTS obs_comments_from_temp;
+#
+CREATE FUNCTION obs_comments_from_temp(_encounterId int(11), _source varchar(50), _term varchar(255), _source1 varchar(50), _term1 varchar(255))
+    RETURNS text
+    DETERMINISTIC
+
+BEGIN
+
+    DECLARE ret text;
+
+    select      o.comments into ret
+    from        temp_obs o
+    where       o.voided = 0
+    and         o.encounter_id = _encounterId
+    and         o.concept_id = concept_from_mapping(_source, _term)
+    and 		o.value_coded = concept_from_mapping(_source1, _term1)
+    order by    o.date_created desc, o.obs_id desc
+    limit 1;
+
+    RETURN ret;
+
+END
+#
+-- This function accepts obs_group_id, mapping source, mapping code
+-- It will find the value_coded entries that match this, separated by |
+-- this function runs against the temp_obs table
+#
+DROP FUNCTION IF EXISTS obs_from_group_id_value_coded_list_from_temp;
+#
+CREATE FUNCTION obs_from_group_id_value_coded_list_from_temp(_obsGroupId int(11), _source varchar(50), _term varchar(255), _locale varchar(50))
+    RETURNS text
+    DETERMINISTIC
+
+BEGIN
+
+    DECLARE ret text;
+
+    select      group_concat(distinct concept_name(o.value_coded, _locale) separator ' | ') into ret
+    from        temp_obs o
+    where       o.voided = 0
+      and       o.obs_group_id= _obsGroupId
+      and       o.concept_id = concept_from_mapping(_source, _term);
+
+    RETURN ret;
+
+END
+#
+-- This function accepts encounter_id, mapping source, mapping code
+-- It will find a single, best observation that matches this, and return the value_numeric
+-- this function runs against the temp_obs table
+#
+DROP FUNCTION IF EXISTS obs_value_numeric_from_temp;
+#
+CREATE FUNCTION obs_value_numeric_from_temp(_encounterId int(11), _source varchar(50), _term varchar(255))
+RETURNS double
+DETERMINISTIC
+
+BEGIN
+
+DECLARE ret double;
+
+select      o.value_numeric into ret
+from        temp_obs o
+where       o.voided = 0
+and         o.encounter_id = _encounterId
+and         o.concept_id = concept_from_mapping(_source, _term)
+order by    o.date_created desc, o.obs_id desc
+limit 1;
+
+RETURN ret;
+
+END
+#
