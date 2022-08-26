621185c095d8a7b7aafff5b5999a6a58cf678d51
dmdesimone <ddesimone@pih.org>
2022-08-25 21:31:29 -0400

    uhm-6540 updated pathology export and ETL

--- configuration/reports/reportdescriptors/dataexports/sql/pathology.sql
+++ configuration/reports/reportdescriptors/dataexports/sql/pathology.sql
@@ -2,6 +2,7 @@
 -- set @endDate = '2022-07-01';
 
 CALL initialize_global_metadata();
+set @partition = '${partitionNum}';
 
 select encounter_role_id into @ordering_provider from encounter_role where uuid = 'c458d78e-8374-4767-ad58-9f8fe276e01c';
 select encounter_role_id into @assisting_surgeon from encounter_role where uuid = '6e630e03-5182-4cb3-9a82-a5b1a85c09a7';
@@ -10,51 +11,57 @@ select encounter_role_id into @attending_surgeon from encounter_role where uuid 
 DROP TEMPORARY TABLE IF EXISTS temp_pathology;
 CREATE TEMPORARY TABLE temp_pathology
 (
-order_id                    INT(11),
-order_number                VARCHAR(50),
-encounter_id                INT(11),
-patient_id                  INT(11),
-zlemrid                     VARCHAR(50),
-loc_registered              VARCHAR(255),
-patient_name                VARCHAR(255),
-unknown_patient             VARCHAR(50),
-gender                      VARCHAR(50),
-age_at_enc                  INT,
-department                  VARCHAR(255),
-commune                     VARCHAR(255),
-section                     VARCHAR(255),
-locality                    VARCHAR(255),
-street_landmark             VARCHAR(255),
-order_datetime              DATETIME,
-ordering_provider           VARCHAR(255),
-request_coded_proc1         VARCHAR(255),
-request_coded_proc2         VARCHAR(255),
-request_coded_proc3         VARCHAR(255),
-request_non_coded_proc      TEXT,	
-prepath_dx                  VARCHAR(255),
-clinical_history            TEXT,
-specimen_accession_number   TEXT,
-post_op_diagnosis           VARCHAR(255),
-specimen_details_1          TEXT,
-specimen_details_2          TEXT,
-specimen_details_3          TEXT,
-specimen_details_4          TEXT,
-specimen_details_5          TEXT,
-specimen_details_6          TEXT,
-specimen_details_7          TEXT,
-specimen_details_8          TEXT,
-attending_surgeon           VARCHAR(255),
-resident                    VARCHAR(255),
-md_to_notify                TEXT,
-clinician_telephone         TEXT,
-urgent_review               VARCHAR(255),
-suspected_cancer            VARCHAR(255),
-immunohistochemistry_needed VARCHAR(255),
-immunohistochemistry_sent   VARCHAR(255),
-date_sent                   DATETIME,
-results_date                DATETIME,
-results_note                TEXT,
-file_uploaded               VARCHAR(255)
+patient_id					INT(11),
+emr_id						VARCHAR(50),
+age_at_enc					INT,
+gender						VARCHAR(50),
+order_id					INT(11),
+order_number 				VARCHAR(50),
+encounter_id				INT(11),
+encounter_location			VARCHAR(255),
+order_datetime				DATETIME,
+order_entered_datetime		DATETIME,
+order_user_entered			VARCHAR(100),
+ordering_provider			VARCHAR(255),
+attending_surgeon			VARCHAR(255),
+resident					VARCHAR(255),
+md_to_notify				TEXT,
+clinician_telephone			TEXT,
+prepath_dx					VARCHAR(255),
+clinical_history			TEXT,
+post_op_diagnosis			VARCHAR(255),	
+specimen_details_1			TEXT,
+specimen_details_2			TEXT,
+specimen_details_3			TEXT,
+specimen_details_4			TEXT,
+specimen_details_5			TEXT,
+specimen_details_6			TEXT,
+specimen_details_7			TEXT,
+specimen_details_8			TEXT,
+urgent_review				BIT,
+suspected_cancer			BIT,
+immunohistochemistry_needed	BIT,
+immunohistochemistry_sent	BIT,
+immunohistochemistry_sent_date	DATETIME,
+latest_processed_obs_id		INT(11),
+process_date				DATETIME,
+process_date_entered		DATETIME,	
+process_user_entered		VARCHAR(50),
+specimen_accession_number	TEXT,
+specimen_to_boston			BIT,
+specimen_to_boston_date		DATETIME,
+specimen_to_pop_obs_groupid	INT(11),
+specimen_to_pap				BIT,
+specimen_to_pap_date		DATETIME,
+specimen_returned_pap_date	DATETIME,
+latest_results_obs_id		INT(11),
+results_date				DATETIME,
+result_date_entered			DATETIME,
+result_user_entered			VARCHAR(255),
+results_note				TEXT,
+file_uploaded				BIT,
+cancer_confirmed			BIT,
+post_result_diagnosis		VARCHAR(255)
 );
 
 insert into temp_pathology (order_id,encounter_id, order_number, patient_id, prepath_dx)
@@ -64,17 +71,18 @@ AND ((date(o.date_activated) >= @startDate) or  @startDate is null)
 AND ((date(o.date_activated) <= @endDate) or @endDate is null)
 ;
 
-update temp_pathology t
-set t.zlemrid = zlemr(patient_id);
+DROP TEMPORARY TABLE IF EXISTS temp_obs;
+create temporary table temp_obs 
+select o.obs_id, o.voided ,o.obs_group_id , o.encounter_id, o.person_id, o.concept_id, o.value_coded, o.value_numeric, o.value_text,o.value_datetime, o.comments,o.date_created , o.creator  
+from obs o
+inner join temp_pathology t on t.encounter_id = o.encounter_id
+where o.voided = 0;
 
+create index temp_obs_oi on temp_obs(obs_id);
+create index temp_obs_ci1 on temp_obs(encounter_id, concept_id);
+create index temp_obs_ci2 on temp_obs(obs_group_id, concept_id);
 update temp_pathology t
-set t.patient_name = person_name(t.patient_id);
-
-update temp_pathology t
-set t.loc_registered  = loc_registered(t.patient_id);
-
-update temp_pathology t
-set unknown_patient = unknown_patient(t.patient_id);
+set t.emr_id = zlemr(patient_id);
 
 update temp_pathology t
 set gender = gender(t.patient_id);
@@ -83,19 +91,13 @@ update temp_pathology t
 set age_at_enc = age_at_enc(t.patient_id, t.encounter_id);
 
 update temp_pathology t
-set department = person_address_state_province(t.patient_id);
+set encounter_location = encounter_location_name(t.encounter_id); 
 
 update temp_pathology t
-set commune = person_address_city_village(t.patient_id);
+set order_entered_datetime = encounter_date_created(t.encounter_id);
 
 update temp_pathology t
-set section = person_address_three(t.patient_id);
-
-update temp_pathology t
-set locality = person_address_one(t.patient_id);
-
-update temp_pathology t
-set street_landmark = person_address_two(t.patient_id); 
+set order_user_entered = encounter_creator_name(t.encounter_id);
 
 update temp_pathology t
 set order_datetime  = encounter_date(encounter_id);
@@ -104,49 +106,34 @@ update temp_pathology t
 set ordering_provider = provider_name_of_type(t.encounter_id , @ordering_provider,0);
 
 update temp_pathology t
-set request_coded_proc1 = value_coded_name(obs_id(t.encounter_id, 'PIH','10770',0),@locale);
+set clinical_history  =  obs_value_text_from_temp(t.encounter_id, 'PIH','10142'); 
 
 update temp_pathology t
-set request_coded_proc2 = value_coded_name(obs_id(t.encounter_id, 'PIH','10770',1),@locale);
+set post_op_diagnosis  = obs_from_group_id_value_coded_list_from_temp(obs_id(t.encounter_id, 'PIH','10782',0), 'PIH','3064',@locale);
 
 update temp_pathology t
-set request_coded_proc3 = value_coded_name(obs_id(t.encounter_id, 'PIH','10770',3),@locale);
+set specimen_details_1 = obs_value_text_from_temp(t.encounter_id, 'PIH','10775'); 
 
 update temp_pathology t
-set request_non_coded_proc = obs_value_text(t.encounter_id, 'PIH','10772'); 
+set specimen_details_2 = obs_value_text_from_temp(t.encounter_id, 'PIH','10776'); 
 
 update temp_pathology t
-set clinical_history  =  obs_value_text(t.encounter_id, 'PIH','10142'); 
+set specimen_details_3 = obs_value_text_from_temp(t.encounter_id, 'PIH','10777'); 
 
 update temp_pathology t
-set specimen_accession_number = obs_value_text(t.encounter_id, 'PIH','10840'); 
+set specimen_details_4 = obs_value_text_from_temp(t.encounter_id, 'PIH','10778');  
 
 update temp_pathology t
-set post_op_diagnosis  = obs_from_group_id_value_coded_list(obs_id(t.encounter_id, 'PIH','10782',0), 'PIH','3064',@locale);
+set specimen_details_5 = obs_value_text_from_temp(t.encounter_id, 'PIH','14317');  
 
 update temp_pathology t
-set specimen_details_1 = obs_value_text(t.encounter_id, 'PIH','10775'); 
+set specimen_details_6 = obs_value_text_from_temp(t.encounter_id, 'PIH','14318'); 
 
 update temp_pathology t
-set specimen_details_2 = obs_value_text(t.encounter_id, 'PIH','10776'); 
+set specimen_details_7 = obs_value_text_from_temp(t.encounter_id, 'PIH','14319'); 
 
 update temp_pathology t
-set specimen_details_3 = obs_value_text(t.encounter_id, 'PIH','10777'); 
-
-update temp_pathology t
-set specimen_details_4 = obs_value_text(t.encounter_id, 'PIH','10778');  
-
-update temp_pathology t
-set specimen_details_5 = obs_value_text(t.encounter_id, 'PIH','14317');  
-
-update temp_pathology t
-set specimen_details_6 = obs_value_text(t.encounter_id, 'PIH','14318'); 
-
-update temp_pathology t
-set specimen_details_7 = obs_value_text(t.encounter_id, 'PIH','14319'); 
-
-update temp_pathology t
-set specimen_details_8 = obs_value_text(t.encounter_id, 'PIH','14320'); 
+set specimen_details_8 = obs_value_text_from_temp(t.encounter_id, 'PIH','14320'); 
 
 update temp_pathology t
 set attending_surgeon  =  provider_name_of_type(t.encounter_id, @attending_surgeon, 0); 
@@ -155,60 +142,158 @@ update temp_pathology t
 set resident  =  provider_name_of_type(t.encounter_id, @assisting_surgeon, 0); 
 
 update temp_pathology t
-set md_to_notify  = obs_value_text(t.encounter_id, 'PIH','10779'); 
+set md_to_notify  = obs_value_text_from_temp(t.encounter_id, 'PIH','10779'); 
 
 update temp_pathology t
-set clinician_telephone  = obs_value_text(t.encounter_id, 'PIH','6589'); 
+set clinician_telephone  = obs_value_text_from_temp(t.encounter_id, 'PIH','6589'); 
 
 update temp_pathology t
-set urgent_review = obs_value_coded_list(t.encounter_id, 'PIH','7813',@locale);
+set urgent_review = value_coded_as_boolean(obs_id_from_temp(t.encounter_id, 'PIH','7813',0));
 
 update temp_pathology t
-set suspected_cancer = obs_value_coded_list(t.encounter_id, 'PIH','14111',@locale);
+set suspected_cancer = value_coded_as_boolean(obs_id_from_temp(t.encounter_id, 'PIH','14111',0));
 
 update temp_pathology t
-set immunohistochemistry_needed = obs_value_coded_list(t.encounter_id, 'PIH','14209',@locale);
+set immunohistochemistry_needed = value_coded_as_boolean(obs_id_from_temp(t.encounter_id, 'PIH','14209',0));
 
 update temp_pathology t
-set immunohistochemistry_sent = obs_value_coded_list(t.encounter_id, 'PIH','7818',@locale);
+set immunohistochemistry_sent = value_coded_as_boolean(obs_id_from_temp(t.encounter_id, 'PIH','7818',0));
 
 update temp_pathology t
-set date_sent = obs_value_datetime(t.encounter_id, 'PIH','14239');
+set immunohistochemistry_sent_date = obs_value_datetime_from_temp(t.encounter_id, 'PIH','14239');
+									 
 
 update temp_pathology t
-set results_date = obs_value_datetime(t.encounter_id, 'PIH','10783');
+set process_date = obs_value_datetime_from_temp(t.encounter_id, 'PIH','10485');
+
+set @process_date_id = concept_from_mapping('PIH','10485');
+set @accession_number_id = concept_from_mapping('PIH','10840');
+set @specimen_sent_id = concept_from_mapping('PIH','7818');
+set @specimen_sent_date_id = concept_from_mapping('PIH','14239');
+set @specimen_returned_date_id = concept_from_mapping('PIH','6110');
 
 update temp_pathology t
-set results_note = obs_value_text(t.encounter_id, 'PIH','7907');
+set t.process_date_entered = 
+	(select max(o.date_created) from temp_obs o 
+	where o.encounter_id = t.encounter_id
+	and o.voided= 0
+	and o.concept_id in (@process_date_id , @accession_number_id,  @specimen_sent_id,  @specimen_sent_date_id, @specimen_returned_date_id) );
 
 update temp_pathology t
-set file_uploaded = if(obs_id(t.encounter_id,'PIH','10785',0) is null,0,1 );
+set t.latest_processed_obs_id = 
+	(select obs_id from temp_obs o 
+	where o.encounter_id = t.encounter_id
+	and o.voided= 0
+	and o.concept_id in (@process_date_id , @accession_number_id,  @specimen_sent_id,  @specimen_sent_date_id, @specimen_returned_date_id)
+	and o.date_created =t.process_date_entered
+	limit 1);
+
+update temp_pathology t
+inner join temp_obs o on o.obs_id = t.latest_processed_obs_id
+inner join users u on o.creator  = u.user_id 
+inner join person_name pn on pn.person_id = u.person_id 
+set process_user_entered = concat(given_name, ' ', family_name);
+
+update temp_pathology t
+set specimen_accession_number = obs_value_text_from_temp(t.encounter_id, 'PIH','10840'); 
+
+update temp_pathology t
+inner join temp_obs o on o.encounter_id = t.encounter_id
+	and o.voided = 0
+	and o.concept_id = concept_from_mapping ('PIH','7818')
+set t.specimen_to_boston = 
+	CASE o.value_coded
+		WHEN concept_from_mapping('PIH','YES') then 1
+		WHEN concept_from_mapping('PIH','NO') then 0
+	END  ;
+
+update temp_pathology t
+inner join temp_obs o on o.encounter_id = t.encounter_id
+	and o.voided = 0
+	and o.obs_group_id is null -- the "sent to Boston" data is not in a construct, like "sent to PAP" data is 
+	and o.concept_id = concept_from_mapping ('PIH','14239')
+set t.specimen_to_boston_date = o.value_datetime  ;
+
+update temp_pathology t
+set specimen_to_pop_obs_groupid = obs_id_from_temp(t.encounter_id, 'PIH','14315',0);
+
+update temp_pathology t
+inner join temp_obs o on o.obs_group_id = t.specimen_to_pop_obs_groupid
+	and o.concept_id = concept_from_mapping('PIH','7817')
+set specimen_to_pap =
+	CASE o.value_coded
+		WHEN concept_from_mapping('PIH','YES') then 1
+		WHEN concept_from_mapping('PIH','NO') then 0
+	END
+;
+
+update temp_pathology t
+set specimen_to_pap_date = obs_from_group_id_value_datetime_from_temp(t.specimen_to_pop_obs_groupid,'PIH','14239');
+
+update temp_pathology t
+set specimen_returned_pap_date = obs_from_group_id_value_datetime_from_temp(t.specimen_to_pop_obs_groupid,'PIH','6110');
+
+set @results_note_id = concept_from_mapping('PIH','7907');
+set @results_date_id = concept_from_mapping('PIH','10783');
+set @post_result_dx_id = concept_from_mapping('PIH','1379');
+set @file_uploaded_id = concept_from_mapping('PIH','10785');
+set @confirmed_cancer_id = concept_from_mapping('PIH','14313');
+
+update temp_pathology t
+set t.result_date_entered = 
+	(select max(o.date_created) from temp_obs o 
+	where o.encounter_id = t.encounter_id
+	and o.voided= 0
+	and o.concept_id in (@results_note_id , @results_date_id,  @post_result_dx_id,  @file_uploaded_id, @confirmed_cancer_id) );
+
+update temp_pathology t
+set t.latest_results_obs_id = 
+	(select obs_id from temp_obs o 
+	where o.encounter_id = t.encounter_id
+	and o.voided= 0
+	and o.concept_id in (@results_note_id , @results_date_id,  @post_result_dx_id,  @file_uploaded_id, @confirmed_cancer_id)
+	and o.date_created =t.result_date_entered
+	limit 1);
+
+update temp_pathology t
+inner join temp_obs o on o.obs_id = t.latest_results_obs_id
+inner join users u on o.creator  = u.user_id 
+inner join person_name pn on pn.person_id = u.person_id 
+set result_user_entered = concat(given_name, ' ', family_name);
+
+update temp_pathology t
+set results_date = obs_value_datetime_from_temp(t.encounter_id, 'PIH','10783');
+
+update temp_pathology t
+inner join temp_obs o on o.encounter_id = t.encounter_id
+	and o.voided = 0
+	and o.concept_id = concept_from_mapping ('PIH','7907')
+set t.results_note = o.value_text ;
+
+update temp_pathology t
+set file_uploaded = if(obs_id_from_temp(t.encounter_id,'PIH','10785',0) is null,0,1 );
+
+update temp_pathology t
+set cancer_confirmed = value_coded_as_boolean(obs_id_from_temp(t.encounter_id, 'PIH','14313',0));
 
 select 
-order_id,
-order_number ,
-encounter_id,
-patient_id,
-zlemrid,
-loc_registered,
-patient_name,
-unknown_patient,
-gender,
+emr_id,
 age_at_enc,
-department,
-commune,
-section,
-locality,
-street_landmark,
+gender,
+concat(@partition,'-',order_id),
+concat(@partition,'-',order_number) ,
+concat(@partition,'-',encounter_id),
+encounter_location,
 order_datetime,
+order_entered_datetime,
+order_user_entered,
 ordering_provider,
-request_coded_proc1,
-request_coded_proc2,
-request_coded_proc3,
-request_non_coded_proc,
+attending_surgeon,
+resident,
+md_to_notify,
+clinician_telephone,
 prepath_dx,
 clinical_history,
-specimen_accession_number,
 post_op_diagnosis,
 specimen_details_1,
 specimen_details_2,
@@ -218,16 +303,25 @@ specimen_details_5,
 specimen_details_6,
 specimen_details_7,
 specimen_details_8,
-attending_surgeon,
-resident,
-md_to_notify,
-clinician_telephone,
 urgent_review,
 suspected_cancer,
 immunohistochemistry_needed,
 immunohistochemistry_sent,
-date_sent,
+immunohistochemistry_sent_date,
+process_date,
+process_date_entered,
+process_user_entered,
+specimen_accession_number,
+specimen_to_boston,
+specimen_to_boston_date,
+specimen_to_pap,
+specimen_to_pap_date,
+specimen_returned_pap_date,
 results_date,
+result_date_entered,
+result_user_entered,
 results_note,
-file_uploaded
-from temp_pathology ;
+file_uploaded,
+cancer_confirmed,
+post_result_diagnosis
+from temp_pathology;
