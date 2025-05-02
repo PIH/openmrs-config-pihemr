a09f43334ffe53404ae28a45120bf60d16df6019
dmdesimone <ddesimone@pih.org>
2025-05-02 12:51:43 -0400

    SL-943 rework all_medication_dispensing (#601)

--- configuration/reports/reportdescriptors/dataexports/sql/all_medication_dispensing.sql
+++ configuration/reports/reportdescriptors/dataexports/sql/all_medication_dispensing.sql
@@ -4,30 +4,33 @@ SET @partition = '${partitionNum}';
 DROP TABLE IF EXISTS all_medication_dispensing;
 CREATE TEMPORARY TABLE all_medication_dispensing
 (
-patient_id int,
-obs_group_id int,
-form varchar(10),
-emr_id varchar(50),
-encounter_id int,
-encounter_datetime datetime,
-encounter_location varchar(100),
-date_entered date,
-user_entered varchar(30),
-encounter_provider varchar(30),
-drug_id int(11),
-drug_name varchar(500),
-drug_openboxes_code int,
-duration int,
-duration_unit varchar(20),
-quantity_per_dose double,
-dose_unit text,
-frequency varchar(50),
-quantity_dispensed int,
-quantity_unit varchar(30),
-order_id int, 
-dispensing_status varchar(50),
-status_reason varchar(50),
-instructions text
+patient_id          int,          
+obs_group_id        int,          
+form                varchar(10),  
+emr_id              varchar(50),  
+encounter_id        int,          
+encounter_datetime  datetime,     
+location_id         int(11),      
+encounter_location  varchar(100), 
+date_entered        date,         
+user_entered        varchar(30),  
+creator             int(11),      
+encounter_provider  text,         
+dispenser           int(11),      
+drug_id             int(11),      
+drug_name           varchar(500), 
+drug_openboxes_code int,          
+duration            int,          
+duration_unit       varchar(20),  
+quantity_per_dose   double,       
+dose_unit           text,         
+frequency           varchar(50),  
+quantity_dispensed  int,          
+quantity_unit       varchar(30),  
+order_id            int,           
+dispensing_status   varchar(50),  
+status_reason       varchar(50),  
+instructions        text          
 );
 
 set @dispensing_construct =  concept_from_mapping('PIH','9070');
@@ -49,6 +52,7 @@ AND o.voided = 0;
 
 create index med_encounter_id on all_medication_dispensing(encounter_id);
 create index med_obs_group on all_medication_dispensing(obs_group_id);
+create index med_patient_id on all_medication_dispensing(patient_id);
 
 -- copy all distinct encounters to a row-per-encounter table to update the encounter-level columns
 DROP TABLE IF EXISTS temp_encounter;
@@ -56,12 +60,11 @@ CREATE TEMPORARY TABLE temp_encounter
 (
 encounter_id 			int(11),
 encounter_datetime		datetime,
-encounter_location_id   int(11),	
-encounter_location      varchar(255),
+location_id             int(11),
 date_entered 			date,
 creator					int(11),
-user_entered            varchar(255),
-encounter_provider 		varchar(255)
+encounter_provider      text,
+user_entered            varchar(255)
 );
 
 insert into temp_encounter (encounter_id)
@@ -69,49 +72,25 @@ select distinct encounter_id from all_medication_dispensing;
 
 create index temp_encounter_encounter_id on temp_encounter(encounter_id);
 
-update temp_encounter 
-set encounter_provider = provider(encounter_id);
-
 update temp_encounter t
 inner join encounter e on t.encounter_id = e.encounter_id 
 set t.encounter_datetime = e.encounter_datetime,
-	t.encounter_location_id = e.location_id,
 	t.date_entered = e.date_created ,
-	t.creator = e.creator ;
+	t.creator = e.creator,
+	t.location_id = e.location_id;
 
-update temp_encounter 
-set encounter_location = location_name(encounter_location_id);
-
-update temp_encounter 
-set user_entered =  person_name_of_user(creator);
+update temp_encounter t
+set encounter_provider = provider(encounter_id);
 
 update all_medication_dispensing md
 inner join temp_encounter t on md.encounter_id = t.encounter_id
 set md.encounter_datetime = t.encounter_datetime,
-	md.encounter_location = t.encounter_location,
 	md.date_entered = t.date_entered,
 	md.user_entered = t.user_entered,
+	md.creator = t.creator,
+	md.location_id = t.location_id,
 	md.encounter_provider = t.encounter_provider;
 
--- update emr ids 
--- copy all distinct patients to a row-per-encounter table
-DROP TABLE IF EXISTS temp_emr_ids;
-CREATE TEMPORARY TABLE temp_emr_ids
-(patient_id int(11),
-emr_id		varchar(50)
-);
-
-insert into temp_emr_ids (patient_id)
-select distinct patient_id from all_medication_dispensing;
-
-create index temp_emr_ids_patient_id on temp_emr_ids(patient_id);
-
-UPDATE temp_emr_ids
-SET emr_id=PATIENT_IDENTIFIER(patient_id, METADATA_UUID('org.openmrs.module.emrapi', 'emr.primaryIdentifierType')); 
-
-update all_medication_dispensing md
-inner join temp_emr_ids ei on ei.patient_id = md.patient_id
-set md.emr_id = ei.emr_id;
 
 -- create a reduced obs table with only rows for the dispensing obs groups for all of the obs-level columns
 drop temporary table if exists temp_obs;
@@ -122,6 +101,7 @@ inner join all_medication_dispensing t on t.obs_group_id = o.obs_group_id
 where o.voided = 0;
 
 create index temp_obs_obs_ci on temp_obs(obs_group_id, concept_id);
+create index temp_obs_obs_grp on temp_obs(obs_group_id);
 
 -- collate and decode observations to each obs_group
 set @dose = concept_from_mapping('PIH','9073');
@@ -163,6 +143,87 @@ set t.duration = o.duration,
 	t.drug_id = o.drugId,
 	t.instructions = o.inxs;
 
+-- New Form --------
+set @complete_status = concept_from_mapping('PIH','1267');
+INSERT INTO all_medication_dispensing(form, patient_id, encounter_datetime, date_entered,  creator, dispenser, location_id, drug_id,
+quantity_per_dose,dose_unit, frequency,quantity_dispensed, quantity_unit, order_id, instructions)
+SELECT 
+'New' AS form,
+patient_id,
+date_handed_over,
+md.date_created,
+md.creator,
+dispenser,
+location_id,
+drug_id ,
+dose quantity_per_dose,
+concept_name(dose_units,'en') dose_units,
+concept_name(of2.concept_id ,'en') frequency,
+quantity quantity_dispensed,
+concept_name(quantity_units,'en') AS quantity_unit, 
+drug_order_id AS order_id, 
+dosing_instructions prescription
+FROM medication_dispense md 
+LEFT OUTER JOIN order_frequency of2 ON of2.order_frequency_id = md.frequency
+where md.status = @complete_status;
+
+-- update location
+update all_medication_dispensing m
+set encounter_location = location_name(location_id);
+
+-- user names of creator, dispenser
+-- copy all distinct creators and dispensers to a table, find the name and join back to main table
+drop temporary table if exists temp_user_names;
+CREATE TEMPORARY TABLE temp_user_names
+(user_id  int(11),
+user_name text
+);
+
+drop temporary table if exists creators;
+create temporary table creators
+select distinct creator from all_medication_dispensing;
+
+insert into temp_user_names (user_id)
+select user_id from (
+select creator as user_id from creators
+union 
+select distinct dispenser as user_id from all_medication_dispensing) i;
+
+create index temp_user_names_ui on temp_user_names(user_id);
+
+update temp_user_names 
+set user_name =  person_name_of_user(user_id);
+
+update all_medication_dispensing md
+inner join temp_user_names u on md.creator = u.user_id
+set md.user_entered = u.user_name;
+
+update all_medication_dispensing md
+inner join temp_user_names u on md.dispenser = u.user_id
+set md.encounter_provider = u.user_name
+where dispenser is not null;
+
+-- update emr ids 
+-- copy all distinct patients to a row-per-encounter table
+DROP TEMPORARY TABLE IF EXISTS temp_emr_ids;
+CREATE TEMPORARY TABLE temp_emr_ids
+(patient_id int(11),
+emr_id		varchar(50)
+);
+
+insert into temp_emr_ids (patient_id)
+select distinct patient_id from all_medication_dispensing;
+
+create index temp_emr_ids_patient_id on temp_emr_ids(patient_id);
+
+set @primary_emr_id = METADATA_UUID('org.openmrs.module.emrapi', 'emr.primaryIdentifierType');
+UPDATE temp_emr_ids
+SET emr_id=PATIENT_IDENTIFIER(patient_id, @primary_emr_id); 
+
+update all_medication_dispensing md
+inner join temp_emr_ids ei on ei.patient_id = md.patient_id
+set md.emr_id = ei.emr_id;
+
 -- -- copy all distinct drugs to a row-per-drug table to update the drug level columns
 DROP TABLE IF EXISTS temp_drug_ids;
 CREATE TEMPORARY TABLE temp_drug_ids
@@ -187,30 +248,6 @@ inner join temp_drug_ids t on t.drug_id = tgt.drug_id
 set tgt.drug_name = t.drug_name,
 	tgt.drug_openboxes_code = t.drug_openboxes_code;
 
--- New Form --------
-INSERT INTO all_medication_dispensing(form, patient_id, emr_id,encounter_datetime, user_entered,drug_name, drug_openboxes_code,
-quantity_per_dose,dose_unit, frequency,quantity_dispensed, quantity_unit, order_id, dispensing_status, status_reason,instructions)
-SELECT 
-'New' AS form,
-patient_id,
-PATIENT_IDENTIFIER(patient_id, METADATA_UUID('org.openmrs.module.emrapi', 'emr.primaryIdentifierType')) emr_id,
-md.date_created date_entered,
-u.username user_entered,
-drugName(drug_id) drug_name,
-openboxesCode(drug_id) drug_openboxes_code,
-dose quantity_per_dose,
-concept_name(dose_units,'en') dose_units,
-concept_name(of2.concept_id ,'en') frequency,
-quantity quantity_dispensed,
-concept_name(quantity_units,'en') AS quantity_unit, 
-drug_order_id AS order_id, 
-concept_name(status, 'en') AS dispensing_status,
-concept_name(status_reason, 'en') AS status_reason,
-dosing_instructions prescription
-FROM medication_dispense md 
-LEFT OUTER JOIN users u ON md.creator=u.user_id
-LEFT OUTER JOIN order_frequency of2 ON of2.order_frequency_id = md.frequency;
-
 -- final select of the data
 SELECT 
 form,
@@ -219,6 +256,7 @@ emr_id,
 CONCAT(@partition,'-',encounter_id) "encounter_id",
 encounter_datetime,
 encounter_location,
+date_entered,
 user_entered,
 encounter_provider,
 drug_name,
@@ -231,7 +269,5 @@ frequency,
 quantity_dispensed,
 quantity_unit,
 order_id,
-dispensing_status,
-status_reason,
 instructions
 FROM all_medication_dispensing;
