16815abf8ef0d07fef98fb802e6560bc8d247022
Chi Bong Ho <cbho@pih.org>
2025-06-27 15:37:41 -0400

    update hotjar id

--- configuration/pih/scripts/global/hotjar.js
+++ configuration/pih/scripts/global/hotjar.js
@@ -1,12 +1,12 @@
 // hotjar is a 3rd party behavioral analytics tool that allows us to record user UI 
 // interactions. We enable it at selected training servers
-if("sle-kgh-inf-omrstest" === window.location.host) {
-  (function(h,o,t,j,a,r){
-      h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-      h._hjSettings={hjid:6424451,hjsv:6};
-      a=o.getElementsByTagName('head')[0];
-      r=o.createElement('script');r.async=1;
-      r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
-      a.appendChild(r);
-  })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+if("sle-kgh-omrstest.pihelp.org" === window.location.host) {
+    (function(h,o,t,j,a,r){
+        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+        h._hjSettings={hjid:6444833,hjsv:6};
+        a=o.getElementsByTagName('head')[0];
+        r=o.createElement('script');r.async=1;
+        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+        a.appendChild(r);
+    })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
 }
