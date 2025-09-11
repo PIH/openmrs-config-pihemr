c51c090473463b6d0a3a095b0260e5bd1168801e
Michael Seaton <mseaton@pih.org>
2025-09-11 16:29:40 -0400

    UHM-8774 - Additional iterations

--- configuration/pih/scripts/global/renderLabOrdersByCategory.js
+++ configuration/pih/scripts/global/renderLabOrdersByCategory.js
@@ -8,75 +8,175 @@ function renderLabOrdersByCategory(config) {
     console.debug(config);
 
     const $widgetField = jq('#' + config.fieldName);
-    var $orderSection = $widgetField.find(".orderwidget-order-section");
+    const $orderSection = $widgetField.find(".orderwidget-order-section");
+    const $templateSection = jq('#' + config.fieldName + "_template");
 
-    config.concepts.forEach(function(concept, conceptIndex) {
-        const fieldIndex = conceptIndex+1;
-        const toggleField = "order_toggle_"  + fieldIndex;
-        const toggleInput = jq(document.createElement("input")).prop({id: toggleField, name: toggleField, value: '', type: 'checkbox'});
-        const actionField = config.widgets.action + '_'  + fieldIndex;
-        const actionInput = jq(document.createElement("input")).prop({id: actionField, name: actionField, value: '', type: 'hidden'});
-        const conceptField = config.widgets.concept + '_' + fieldIndex;
-        const conceptInput = jq(document.createElement("input")).prop({id: conceptField, name: conceptField, value: concept.conceptId, type: 'hidden'});
-        const urgencyField = config.widgets.urgency + '_'  + fieldIndex;
-        const urgencyInput = jq(document.createElement("input")).prop({id: urgencyField, name: urgencyField, value: 'ROUTINE', type: 'hidden'});
-        const careSettingField = config.widgets.careSetting + '_'  + fieldIndex;
-        const careSettingInput = jq(document.createElement("input")).prop({id: careSettingField, name: careSettingField, value: 'OUTPATIENT', type: 'hidden'});
-        const previousOrderField = config.widgets.previousOrder + '_'  + fieldIndex;
-        const previousOrderInput = jq(document.createElement("input")).prop({id: previousOrderField, name: previousOrderField, type: 'hidden'});
+    // Determine which fields need to be collected, by examining both the template or default configuration with multiple options
+    const labOrderFields = Object.keys(config.widgets);
 
-        // Determine if there is already an existing order in the encounter for this concept
-        let previousOrderId = null;
-        config.history.forEach(function(order) {
-            if (order.encounterId === config.encounterId && order.concept.value === concept.conceptId) {
-                previousOrderId = order.orderId;
+    // Iterate over each field in the template and determine if it should be displayed or not
+    const templateSections = $templateSection.find(".order-field-widget");
+    const fieldSections = [];
+    templateSections.each(function () {
+        const fieldName = labOrderFields.filter((field) => $(this).hasClass("order-" + field)).at(0);
+        const fieldWidgetSection = $templateSection.find(".order-field-widget.order-" + fieldName);
+        const numRadioInputs = fieldWidgetSection.find("input[type=radio][value != '']").length;
+        const numSelectInputs = fieldWidgetSection.find("select").length;
+        const numSelectOptions = fieldWidgetSection.find("option[value != '']").length;
+        const isInFormTemplate = fieldWidgetSection.parents(".non-template-field").length === 0;
+        fieldSections.push({fieldName, fieldWidgetSection, isInFormTemplate, numRadioInputs, numSelectInputs, numSelectOptions})
+    });
+    console.debug(fieldSections);
+
+    config.labTestCategories.forEach(function(category) {
+
+        // Create section for each category, with the category name, and the category tests
+        const $labCategorySection = jq(document.createElement("div")).attr("id", "lab-category-" + category.conceptId).addClass("lab-category");
+        const $labCategoryNameElement = jq(document.createElement("div")).addClass("lab-category-name").html(category.displayName);
+        const $labCategoryTestsSection = jq(document.createElement("div")).attr("id", "lab-category-tests-" + category.conceptId).addClass("lab-category-tests");
+
+        // Render the tests the given category
+        category.labTests.forEach(function(labTest) {
+
+            const idSuffix = '_' + labTest.conceptId;
+
+            // Determine if there is already an existing order in the encounter for this concept
+            let previousOrder = null;
+            config.history.forEach(function(order) {
+                if (order.encounterId === config.encounterId && order.concept.value === labTest.conceptId) {
+                    previousOrder = order;
+                }
+            });
+
+            const $labSection = jq(document.createElement("div")).attr("id", "lab" + idSuffix).addClass("lab-test-section");
+            $labCategoryTestsSection.append($labSection);
+
+            // Create the tooltip section
+            const $toolTipSection = jq(document.createElement("span")).addClass("panel-tool-tip-section");
+            if (labTest.testsInPanel && labTest.testsInPanel.length > 0) {
+                const $toolTipButton = jq(document.createElement("i")).addClass("icon-info-sign");
+                $toolTipSection.append($toolTipButton);
+                const $toolTipText = jq(document.createElement("span")).addClass("tooltip-text");
+                $toolTipSection.append($toolTipText);
             }
-        });
 
-        const labSection = jq(document.createElement("div"));
-        if (config.mode === 'VIEW') {
-            const labValueSection = jq(document.createElement("span"));
-            if (previousOrderId) {
-                labValueSection.addClass("value").html("[X]&nbsp;" + concept.conceptLabel);
+            // Handle the test concept, action, and previous order fields first
+
+            if (config.mode === 'VIEW') {
+                const labValueSection = jq(document.createElement("span"));
+                if (previousOrder) {
+                    labValueSection.addClass("value").html("[X]&nbsp;" + labTest.displayName);
+                    $labSection.find(".lab-fields").show();
+                }
+                else {
+                    labValueSection.addClass("emptyValue").html("[&nbsp;&nbsp;]&nbsp;" + labTest.displayName);
+                    $labSection.find(".lab-fields").hide();
+                }
+                $labSection.append(labValueSection);
+                $labSection.append($toolTipSection);
             }
             else {
-                labValueSection.addClass("emptyValue").html("[&nbsp;&nbsp;]&nbsp;" + concept.conceptLabel);
-            }
-            labSection.append(labValueSection)
-        }
-        else {
-            labSection.append(toggleInput);
-            labSection.append(conceptInput).append(" " + concept.conceptLabel);
-            labSection.append(actionInput);
-            labSection.append(urgencyInput);
-            labSection.append(careSettingInput);
-            labSection.append(previousOrderInput);
+                const toggleField = "order_toggle"  + idSuffix;
+                const toggleInput = jq(document.createElement("input")).prop({id: toggleField, name: toggleField, value: '', type: 'checkbox'});
+                $labSection.append(toggleInput);
 
-            jq(actionInput).val('');
-            if (previousOrderId) {
-                jq(previousOrderInput).val(previousOrderId);
-                jq(toggleInput).prop("checked", true);
-            }
+                const actionField = config.widgets.action + idSuffix;
+                const actionInput = jq(document.createElement("input")).prop({id: actionField, name: actionField, value: '', type: 'hidden'});
+                $labSection.append(actionInput);
 
-            jq(toggleInput).click(function(event) {
-                if(jq(toggleInput).is(':checked')) {
-                    if (previousOrderId) {
-                        jq(actionInput).val('');
+                const conceptField = config.widgets.concept + idSuffix;
+                const conceptInput = jq(document.createElement("input")).prop({id: conceptField, name: conceptField, value: labTest.conceptId, type: 'hidden'});
+                $labSection.append(conceptInput).append(" " + labTest.displayName);
+                $labSection.append($toolTipSection);
+
+                const previousOrderField = config.widgets.previousOrder + idSuffix;
+                const previousOrderInput = jq(document.createElement("input")).prop({id: previousOrderField, name: previousOrderField, type: 'hidden'});
+                $labSection.append(previousOrderInput);
+
+                // Set up the toggle checkbox to set the order action to either NEW, DISCONTINUE, or null
+                jq(actionInput).val('');
+                if (previousOrder) {
+                    jq(previousOrderInput).val(previousOrder ? previousOrder.orderId : '');
+                    jq(toggleInput).prop("checked", true);
+                }
+                jq(toggleInput).click(function() {
+                    if(jq(toggleInput).is(':checked')) {
+                        jq(actionInput).val(previousOrder ? "" : "NEW");
+                        $labSection.find(".lab-fields").show();
                     }
                     else {
-                        jq(actionInput).val('NEW');
+                        jq(actionInput).val(previousOrder ? "DISCONTINUE" : "");
+                        $labSection.find(".lab-fields").hide();
+                    }
+                });
+            }
+
+            const $labFieldsSection = jq(document.createElement("span")).attr("id", "lab-fields" + idSuffix).addClass("lab-fields");
+            $labSection.append($labFieldsSection);
+            if (previousOrder) {
+                $labFieldsSection.show();
+            }
+            else {
+                $labFieldsSection.hide();
+            }
+
+            const requiredCodedFields = ["careSetting", "urgency"];
+            const nonRequiredCodedFields = ["orderReason", "specimenSource", "laterality", "frequency", "location"];
+            const nonCodedFields = ["orderReasonNonCoded", "instructions", "clinicalHistory", "numberOfRepeats", "scheduledDate", "dateActivated"];
+
+            fieldSections.forEach(function(fieldSection) {
+                const field = fieldSection.fieldName;
+                const $clonedFieldSection = jq(fieldSection.fieldWidgetSection).clone(true, true);
+
+                $clonedFieldSection.find("[id]").add($clonedFieldSection).each(function () {
+                    if (this.id) {
+                        this.id = this.id + idSuffix;
+                    }
+                });
+                $clonedFieldSection.find("[name]").add($clonedFieldSection).each(function () {
+                    if (this.name) {
+                        this.name = this.name + idSuffix;
+                    }
+                });
+
+                // We have special handling for orderReason and urgency, so do these first
+                if (field === "orderReason") {
+                    if (labTest.reasons && labTest.reasons.length > 0) {
+                        const $orderReasonSelect = $clonedFieldSection.find("select");
+                        labTest.reasons.forEach(function(reason) {
+                            $orderReasonSelect.append(jq(document.createElement("option")).attr("value", reason.conceptId).html(reason.displayName));
+                        });
+                        $clonedFieldSection.show();
+                    }
+                    else {
+                        $clonedFieldSection.hide();
                     }
                 }
                 else {
-                    if (previousOrderId) {
-                        jq(actionInput).val('DISCONTINUE');
+                    if (requiredCodedFields.includes(field)) {
+                        if (fieldSection.numRadioInputs > 1 || fieldSection.numSelectOptions > 1) {
+                            $clonedFieldSection.show();
+                        }
+                        else {
+                            $clonedFieldSection.hide();
+                        }
+                        $labFieldsSection.append($clonedFieldSection);
                     }
-                    else {
-                        jq(actionInput).val('');
+                    else if (nonRequiredCodedFields.includes(field) || nonCodedFields.includes(field)) {
+                        if (fieldSection.isInFormTemplate) {
+                            $clonedFieldSection.show();
+                            $labFieldsSection.append($clonedFieldSection);
+                        }
                     }
                 }
             });
-        }
-        $orderSection.append(labSection);
+
+            if (previousOrder) {
+                orderWidget.populateOrderForm(config, $labFieldsSection, previousOrder);
+            }
+        });
+
+        $labCategorySection.append($labCategoryNameElement).append($labCategoryTestsSection);
+        $orderSection.append($labCategorySection);
     });
 }
