0c45c46b3f023f97fa508d38984b22d9830f7382
Cosmin <cioan@pih.org>
2026-05-19 11:35:04 -0400

    SL-1332: migrate standalone syphilis positive RDT test results

--- configuration/pih/liquibase/sql/migrate-syphilis-rapid-tests.sql
+++ configuration/pih/liquibase/sql/migrate-syphilis-rapid-tests.sql
@@ -2,7 +2,9 @@ SET @old_syphilis_test_concept_uuid          = '7ff67b85-7c94-49f6-8187-786fb8f9
 SET @old_syphilis_test_result_value_uuid     = 'ef2bfd0f-db05-450c-8e1c-280a2c4d7a5e';
 SET @new_syphilis_test_obsgroup_concept_uuid = 'ea5d701a-a8ec-48b2-84c3-2df1c25ac080';
 SET @new_syphilis_test_result_concept_uuid   = '165303AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA';
-SET @new_syphilis_test_result_positive_uuid  = '3cd8f3f6-26fe-102b-80cb-0017a47871b2';
+SET @new_syphilis_test_positive_result_value_id  = (SELECT concept_id FROM concept WHERE uuid ='3cd8f3f6-26fe-102b-80cb-0017a47871b2');
+SET @anc_intake_encounter_id  = (select encounter_type_id from encounter_type where uuid= '00e5e810-90ec-11e8-9eb6-529269fb1459');
+SET @anc_followup_encounter_id  = (select encounter_type_id from encounter_type where uuid= '00e5e946-90ec-11e8-9eb6-529269fb1459');
 
 DROP TABLE IF EXISTS tmp_migrate_syphilis_test;
 
@@ -11,7 +13,14 @@ SELECT o.*
 FROM obs o
 WHERE o.voided = 0
   AND o.concept_id  = (SELECT concept_id FROM concept WHERE uuid = @old_syphilis_test_concept_uuid)
-  AND o.value_coded = (SELECT concept_id FROM concept WHERE uuid = @old_syphilis_test_result_value_uuid);
+  AND o.value_coded = (SELECT concept_id FROM concept WHERE uuid = @old_syphilis_test_result_value_uuid)
+UNION
+SELECT o.*
+FROM obs o, encounter e
+WHERE o.encounter_id = e.encounter_id and e.encounter_type in (@anc_intake_encounter_id, @anc_followup_encounter_id)
+  and o.obs_group_id is null and o.voided = 0
+  AND o.concept_id  = (SELECT concept_id FROM concept WHERE uuid = @new_syphilis_test_result_concept_uuid)
+  AND o.value_coded = @new_syphilis_test_positive_result_value_id;
 
 ALTER TABLE tmp_migrate_syphilis_test ADD COLUMN new_obs_group_uuid CHAR(38) DEFAULT NULL;
 ALTER TABLE tmp_migrate_syphilis_test ADD COLUMN new_obs_group_id   INT(11)  DEFAULT NULL;
@@ -36,7 +45,7 @@ INSERT INTO obs
  obs_group_id, date_created, uuid)
 SELECT t.person_id,
        (SELECT concept_id FROM concept WHERE uuid = @new_syphilis_test_result_concept_uuid),
-       (SELECT concept_id FROM concept WHERE uuid = @new_syphilis_test_result_positive_uuid),
+       @new_syphilis_test_positive_result_value_id,
        t.encounter_id, t.obs_datetime, t.location_id, t.creator,
        t.new_obs_group_id,
        NOW(), UUID()
