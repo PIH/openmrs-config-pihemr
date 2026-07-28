7ca5a43ba7b8346f4d7c5b2a40467f592567bc22
mogoodrich <mgoodrich@pih.org>
2026-07-28 15:56:08 -0400

    SL-1390: NCD sections not loading when using custom buttons
    (cherry picked from commit 29a2f0d513e98c24e96346cbffcc24fbd1af61b4)

--- configuration/pih/scripts/global/nextAndSubmitButtons.js
+++ configuration/pih/scripts/global/nextAndSubmitButtons.js
@@ -9,9 +9,25 @@ function setUpNextSectionButtons() {
     jq(".nextSection").click(function () {
         let nextSection = jq(this).attr('id');
         if (nextSection) {
-            window.htmlForm.setReturnUrl(window.htmlForm.getReturnUrl().split('#')[0] + '&nextSection=' + nextSection + '&goToNext=true#' + window.htmlForm.getReturnUrl().split('#')[1]);
-            window.htmlForm.submitHtmlForm();
+            // append nextSection and goToNext=true, unless returnUrl already has them (e.g. left over from a prior validation-error submit), in which case update nextSection in place and don't duplicate goToNext=true
+            let returnUrl = window.htmlForm.getReturnUrl();
+            let hashIndex = returnUrl.indexOf('#');
+            let base = hashIndex === -1 ? returnUrl : returnUrl.substring(0, hashIndex);
+            let hash = hashIndex === -1 ? '' : returnUrl.substring(hashIndex);
+
+            if (base.match(/[&?]nextSection=[^&]*/)) {
+                base = base.replace(/([&?])nextSection=[^&]*/, '$1nextSection=' + nextSection);
+            } else {
+                base = base + '&nextSection=' + nextSection;
+            }
+
+            if (!base.match(/[&?]goToNext=true(&|$)/)) {
+                base = base + '&goToNext=true';
+            }
+
+            window.htmlForm.setReturnUrl(base + hash);
         }
+        window.htmlForm.submitHtmlForm();
     });
 }
 
